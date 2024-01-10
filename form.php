3cc0b42f0a5bcae0deab80c401f4d57db8dda7ee
sreevalli <63503886+sreevalli-2011@users.noreply.github.com>
2024-01-10 09:13:51 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forms</title>
+</head>
+<body>
+
+    <form action="testform.php" method="post">
+        Name: <input type="text" name="fname"><br><br>
+        
+        Age: <input type="text" name="age"><br><br>
+
+        <input type="submit" name="save"><br><br>
+        </form>
+    
+</body>
+</html>
